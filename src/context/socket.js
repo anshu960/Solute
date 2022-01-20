097cc2c36843e0b7fa6217d6a42c8457a1a1d61d
Vivek Singh <ervivek40@gmail.com>
2022-01-20 19:04:35 +0530

    env import

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import server_config from "./../config/server_config";
 import socketio from "socket.io-client";
-import environment from "./../../../environment"
+import environment from "./../../environment"
 // const socket = socketio.connect(process.env.IS_PRODUCTION ? server_config.releaseSocketServer : server_config.devSocketServer);
 const socket = socketio.connect(environment.socket);
 
