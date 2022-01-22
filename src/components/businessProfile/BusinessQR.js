67f61e54af7344de18f56ecf6e59b0c1b58758ef
Vivek Singh <ervivek40@gmail.com>
2022-01-22 08:30:09 +0530

    changed import

--- src/components/businessProfile/BusinessQR.js
+++ src/components/businessProfile/BusinessQR.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 
 import { Box } from '@mui/material';
 import QRCode from "react-qr-code";
 import { getBusinessId } from '../../services/authService';
-
-// ----------------------------------------------------------------------
 
 export default function BusinessQR() {
   
