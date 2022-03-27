f81d7bcafc128bdd85ae680983bccbbef6b151c0
Vivek Singh <ervivek40@gmail.com>
2022-03-27 19:23:24 +0530

    payment popup

--- src/store/subscription.js
+++ src/store/subscription.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 import { getBusiness, getUserId } from '../services/authService';
 import SocketEvent from '../socket/SocketEvent';
 import { SendEvent } from '../socket/SocketHandler';
-
+import ImageConfig from "./../config/image_config";
 const slice = createSlice({
     name: 'Subscription',
     initialState: {
@@ -48,9 +48,50 @@ export const createRazorpayOrder = (amount,callBack) => async dispatch => {
   export const subscribeToPlan = (plan) => async dispatch => {
     try {
         dispatch(createRazorpayOrder(plan.amount,(razorpayOrder)=>{
-
+            showRazorpay(razorpayOrder)
         }));
     } catch (e) {
       return console.error(e.message);
     }
+  }
+
+  const showRazorpay=(transaction)=>{
+    const { amount, id: order_id, currency } = transaction;
+    const business = getBusiness()
+    const options = {
+      key: 'rzp_test_iABvXOSIa0Dv3B', // Enter the Key ID generated from the Dashboard
+      amount: amount.toString(),
+      currency: currency,
+      name: 'Solute',
+      description: 'Membership',
+      image: { uri:ImageConfig.Logo },
+      order_id: order_id,
+      handler: (response) =>{
+        const data = {
+          orderCreationId: order_id,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
+        };
+
+        // const result = await axios.post('/payment/success', data);
+
+        alert(data.msg);
+      },
+      
+      prefill: {
+        name: business.Name,
+        email: business.Email,
+        contact: business.MobileNumber,
+      },
+      notes: {
+        address: business.Address,
+      },
+      theme: {
+        color: '#61dafb',
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
   }
